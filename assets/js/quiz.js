24b1ca879cd78aef20f9b70be91ecdc144f21b72
tejaadusumilli21 <teamteja3@gmail.com>
2025-10-16 16:13:42 +0530

    v8

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -338,7 +338,7 @@ function setupPretestModal() {
 
   function openModal() {
     if (!modal) return;
-    modal.style.display = 'block';
+    modal.style.display = 'flex';
     modal.setAttribute('aria-hidden', 'false');
   }
   function closeModal() {
